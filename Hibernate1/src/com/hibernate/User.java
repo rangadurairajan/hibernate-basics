8509901733ee02f8b27ba6e77412f4fcd00b63eb
rangadurairajan <139765032+rangadurairajan@users.noreply.github.com>
2023-07-17 16:53:20 +0530

    Add files via upload

---
+++ Hibernate1/src/com/hibernate/User.java
@@ -0,0 +1,28 @@
+package com.hibernate;
+
+public class User {
+	private int uid;
+	String uname;
+	String uemail;
+	
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUemail() {
+		return uemail;
+	}
+	public void setUemail(String uemail) {
+		this.uemail = uemail;
+	}
+	
+
+}
